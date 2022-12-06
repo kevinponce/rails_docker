dcd188a4505d227eeec6c6ca4e103f8ed383dba1
Kevin Ponce <kevinponce88@gmail.com>
2022-12-06 17:16:30 -0600

    import @actions github and core

--- .github/actions/setDevelopmentUrl/index.js
+++ .github/actions/setDevelopmentUrl/index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+import { getOctokit } from "@actions/github";
+import { getInput } from "@actions/core";
+
+console.log("@@@@@@@@@")
+
+const token = getInput("token", { required: true, trimWhitespace: true })
+
+console.log(token);
+
+const github = getOctokit(token, {
+  previews: ["ant-man-preview", "flash-preview"],
+});
+
+console.log(github.rest.repos.createDeploymentStatus);
