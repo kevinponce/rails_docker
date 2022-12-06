f02433feef9e70e2625631ba170d1ca0ff40e330
Kevin Ponce <kevinponce88@gmail.com>
2022-12-06 17:49:02 -0600

    aws deploy create github deployment

--- .github/actions/setDevelopmentUrl/index.js
+++ .github/actions/setDevelopmentUrl/index.js
@@ -1,34 +1,46 @@
 import { getOctokit, context } from "@actions/github";
 import { getInput } from "@actions/core";
 
-const token = getInput("token", { required: true, trimWhitespace: true })
+(async () => {
+  const token = getInput("token", { required: true, trimWhitespace: true })
 
-console.log(token);
-console.log(context);
+  console.log(token);
+  console.log(context);
 
-const github = getOctokit(token, {
-  previews: ["ant-man-preview", "flash-preview"],
-});
+  const github = getOctokit(token, {
+    previews: ["ant-man-preview", "flash-preview"],
+  });
 
-const deploymentID = getInput("deployment_id", { required: false, trimWhitespace: true });
-console.log(deploymentID);
+  const deploymentID = getInput("deployment_id", { required: false, trimWhitespace: true });
+  console.log(deploymentID);
 
-console.log(github.rest.repos.createDeploymentStatus);
+  console.log(github.rest.repos.createDeploymentStatus);
 
-// github.rest.repos.createDeploymentStatus({
-//   owner: context.owner,
-//   repo: context.repo,
-//   deployment_id: parseInt(stepArgs.deploymentID, 10),
-//   state: newStatus,
-//   description: description,
-//   ref: context.ref,
 
-//   // only set environment_url if deployment worked
-//   environment_url: newStatus === "success" ? stepArgs.envURL : "",
-//   // set log_url to action by default
-//   log_url: logsURL,
-//   // if we are overriding previous deployments, let GitHub deactivate past
-//   // deployments for us as a fallback, or see if a user explicitly wants to
-//   // use this feature.
-//   auto_inactive: stepArgs.override || stepArgs.autoInactive,
-// });
+  const resp = await octokit.rest.repos.createDeployment({
+    owner: context.owner,
+    repo: context.repo,
+    ref: context.ref,
+  });
+
+  console.log(resp);
+
+
+  // github.rest.repos.createDeploymentStatus({
+  //   owner: context.owner,
+  //   repo: context.repo,
+  //   deployment_id: parseInt(stepArgs.deploymentID, 10),
+  //   state: newStatus,
+  //   description: description,
+  //   ref: context.ref,
+
+  //   // only set environment_url if deployment worked
+  //   environment_url: newStatus === "success" ? stepArgs.envURL : "",
+  //   // set log_url to action by default
+  //   log_url: logsURL,
+  //   // if we are overriding previous deployments, let GitHub deactivate past
+  //   // deployments for us as a fallback, or see if a user explicitly wants to
+  //   // use this feature.
+  //   auto_inactive: stepArgs.override || stepArgs.autoInactive,
+  // });
+})();
