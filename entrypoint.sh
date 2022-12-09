c882d9fedddaf2f6bda8d9397ef2f31e5b95bdde
Kevin Ponce <kevinponce88@gmail.com>
2022-12-09 12:40:51 -0600

    auto run db:migrate on docker start

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /rails_docker/tmp/pids/server.pid
+
+echo "run db:migrate"
+bin/rails db:migrate
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
-
-bin/rails db:migrate
