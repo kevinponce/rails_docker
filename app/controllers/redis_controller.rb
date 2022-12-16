837c21763a4c13d26f8397cdffa62d69c41a4293
Kevin Ponce <kevinponce88@gmail.com>
2022-12-15 20:07:18 -0600

    Added redis service host to webpp env

--- app/controllers/redis_controller.rb
+++ app/controllers/redis_controller.rb
@@ -1,6 +1,6 @@
 class RedisController < ApplicationController
   def index
-    redis = Redis.new(host: ENV['REDIS_HOST'], port: '6379')
+    redis = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_SERVICE_SERVICE_PORT'] || '6379')
     @page_count = (redis.get('count')&.to_i || 0) + 1
 
     redis.set('count', @page_count)
